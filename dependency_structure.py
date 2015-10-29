6e4259a7efdf78440388dba7741f35db88aafbcb
Reid Kilgore <rkilgore@node0.cl.mathcs.emory.edu>
2015-10-29 14:41:27 -0400

    various fixes/updates

---
+++ dependency_structure.py
@@ -0,0 +1,112 @@
+import csv
+
+# Usage: Pass a file name to initialize Dependency_Structure with sentences. 
+# Sentences (node-based structure) can be accessed through Dependency_Structure.
+# Most functionality to examine specfic structure and relations will happen at this level.
+
+class Dependency_Structure:
+    def __init__(self, file_name, limit=None):
+        # accepts name of tsv file containing dependency parsed corpus
+        # track whether the current word (row) is the start of a new sentence
+        last_index = -1
+        # structure to hold sentence objects after processing
+        self.sentences = []
+        # chunk is a list of the words in the current sentence
+        chunk = []
+        with open(file_name, "r") as tsv_in:
+	    tsv_in = csv.reader(tsv_in,delimiter='\t', quotechar='\x07')
+            for row in tsv_in:
+                if limit and len(self.sentences) > limit:
+                    break
+		if not row:
+                    self.sentences.append(Sentence(chunk))
+                    last_index = -1
+                    chunk = []
+                elif row and int(row[0]) > last_index:
+                    last_index = int(row[0])
+                    chunk.append(row)
+                else:
+                    self.sentences.append(Sentence(chunk))
+                    last_index = -1
+                    chunk = [row]
+            if chunk:
+                self.sentences.append(Sentence(chunk))
+
+    def get_sentences(self):
+        return self.sentences
+
+
+class Sentence:
+    def __init__(self, sentence):
+	for s in sentence:
+	    print s
+#        sentence = [term.split('\t') for term in raw_text]
+        nodes = [Node() for i in range(0, len(sentence))]
+        for i, term in enumerate(sentence):
+	    print i
+#	    if term[5] == 'punct':
+#		del nodes[i]
+#		continue # should be end of sentence
+            n = nodes[i]
+            n.set_form(term[1])
+	    print 'think head is ', term[5]
+            n.set_head(int(term[5]), term[6]) # not sure whether to use malt or stanford, this is malt
+            if n.head > 0:
+		h = nodes[n.head]
+            h.add_dep(i, n.arc)
+        self.nodes = nodes
+
+    def get_head(self, index):
+        return self.nodes[self.nodes[index].get_head_index()]
+
+    def get_siblings(self, index):
+        if self.nodes[i].get_head_index() is -1:
+            return []
+        return self.nodes[self.nodes[index].get_head_index()].get_dep_list()
+
+    def get_token_list(self):
+        return [word.get_form() for word in self.nodes]
+
+    def get_adjacency_context(self, which, window=-1):
+        if window < 0:
+            return [word.get_form() for i, word in enumerate(self.nodes) if i is not which]
+        else:
+            context_words = []
+            for i in range(window, which+window):
+                if i > -1 and i is not which and i < len(self.nodes):
+                    context_words.append[self.nodes[i]]
+            return context_words
+
+    def get_dependency_context(self, which, window=-1):
+        # temporary filler
+        return get_head(which)
+
+class Node:
+    def __init__(self):
+        self.head = None
+        self.arc = None
+        self.form = None
+        self.dep = []
+    
+    def set_form(self, form):
+        self.form = form
+
+    def set_head(self, head, arc):
+        self.head = head
+        self.arc = arc
+
+    def get_form():
+        return self.form
+
+    def add_dep(self, index, arc_type):
+        self.dep.append((index, arc_type))
+
+    def get_head_index():
+        return self.head
+        
+    def get_head_arc():
+        return self.arc
+
+    def get_dep_list():
+        return self.dep
+        
--- ds_demo.py
+++ ds_demo.py
@@ -1,3 +1,3 @@
 from dependency_structure import *
 
-sentences = Dependency_Structure(raw_input("Please enter file name:"), limit=50)
+sentences = Dependency_Structure(raw_input("Please enter file name:"), limit=10)
