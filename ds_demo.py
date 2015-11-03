031a12dabe72ba9f39021cf59058b7a26a38d1d3
Reid Kilgore <altkilgore@gmail.com>
2015-11-02 19:20:33 -0500

    fix get tokenized sentences bug

--- ds_demo.py
+++ ds_demo.py
@@ -2,4 +2,5 @@ from dependency_structure import *
 
 # sentences becomes an object of type Dependency_Structure, which mainly manipulates a list of sentences.
 # sentence objects hold a list of nodes that have relationship information available
-sentences = Dependency_Structure(raw_input("Please enter file name:"), limit=10)
+sentences = Dependency_Structure(raw_input("Please enter file name:"))
+sentences.get_tokenized_sentences()
