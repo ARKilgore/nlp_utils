dd77ad0de200780812274d98ae5d4bec5acf750a
arkilgore <altkilgore@gmail.com>
2015-11-24 20:00:49 +0000

    gensim running updates for concurrent file IO

--- runGensim.py
+++ runGensim.py
@@ -1,8 +1,13 @@
 from gensim.models import Word2Vec
+import threaded_reader as tr
+import dependency_structure as dep
+from time import clock
 
-f = open('nytimes.txt', 'r')
-sentences = []
-for line in f.readlines():
-    sentences.append(line)
-model = Word2Vec(sentences, size=100, window=5, min_count=5, workers=4)
-model.save('vectors.out')
+text = tr.t_read(threads=10)
+print 'done reading'
+start = clock()
+ds = dep.Dependency_Structure(text, is_file=False, is_text=True, limit=100)
+sentences = ds.get_tokenized_sentences()
+print 'total ds time: ', str(start - clock())
+model = Word2Vec(sentences, size=100, window=5, min_count=5, workers=16)
+model.save('vectors_nyt.out')
