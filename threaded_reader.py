32bd4d2322123bfe577af79254e502ba7920858d
Reid Kilgore <altkilgore@gmail.com>
2015-11-26 21:30:04 -0500

    run adjacency gensim

--- threaded_reader.py
+++ threaded_reader.py
@@ -10,13 +10,17 @@ prenyt = '/data/mnt/ainos-research/corpus/nytimes/tree/*.cnlp'
 prewiki = '/data/mnt/ainos-research/corpus/wikipedia2015/tree/*.cnlp'
 
 
-def reader(qin, qout, is_build_vocab):
+def reader(qin, qout, is_build_vocab=False, is_build_dep=False):
     name = qin.get()
     while name != None:
-        print 'parsing', name
+        print 'parsing', name[:10]
 	lines = open(name, 'r').readlines()
-        if is_build_vocab:
-            qout.put(dep.Dependency_Structure(lines).unique_words())
+        if is_build_vocab or is_build_dep:
+            ds = dep.Dependency_Structure(lines, is_file=False, is_text=True)
+            if is_build_vocab:
+                qout.put(ds.get_tokenized_sentences())
+            elif is_build_dep:
+                qout.put(ds)
         else:
             qout.put(lines)
         print 'parsed', name
@@ -27,9 +31,9 @@ def t_read(thread_num=100, upper=None, is_build_vocab=False):
     qout = Queue()
     qin = Queue()
     threads = []
-    files = []
-    files.extend(glob.glob(prenyt))
-    files.extend(glob.glob(prewiki))
+    files = ['nyt_000_sentences.cnlp']
+#    files.extend(glob.glob(prenyt))
+#    files.extend(glob.glob(prewiki))
     if upper:
 	files = files[:upper]
     for f in files:
@@ -45,12 +49,15 @@ def t_read(thread_num=100, upper=None, is_build_vocab=False):
 
     text = []
 
-    for _ in range(0, len(files)):
-        text.extend(qin.get())
-        print 'got text, total:', len(text)
-    
     for t in threads:
         t.join()
+
+    for _ in range(0, len(files)):
+        res = qin.get()
+        print res
+        text.extend(res)
+        print 'got text, total:', len(text)
+    
     print 'total time:', str( time.clock() - start )
     print 'returning text', len(text)
     return text
